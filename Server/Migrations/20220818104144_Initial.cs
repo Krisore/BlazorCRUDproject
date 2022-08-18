5a7eb0018f22d081ba469a652dfb769ef0fbdb67
Kristian Paul Enore <kristiandizon.enore@gmail.com>
2022-08-18 20:54:17 +0800

    Add project files.

---
+++ Server/Migrations/20220818104144_Initial.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorFullStackCrud.Server.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Comics",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comics", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SuperHeroes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HeroName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ComicId = table.Column<int>(type: "int", nullable: true),
+                    ComidId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SuperHeroes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SuperHeroes_Comics_ComicId",
+                        column: x => x.ComicId,
+                        principalTable: "Comics",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.InsertData(
+                table: "Comics",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Marvel" },
+                    { 2, "DC" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "SuperHeroes",
+                columns: new[] { "Id", "ComicId", "ComidId", "FirstName", "HeroName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, null, 1, "Peter", "Spiderman", "Parker" },
+                    { 2, null, 2, "Bruce", "batman", "Wayne" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SuperHeroes_ComicId",
+                table: "SuperHeroes",
+                column: "ComicId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SuperHeroes");
+
+            migrationBuilder.DropTable(
+                name: "Comics");
+        }
+    }
+}
